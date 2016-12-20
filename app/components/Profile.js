ff4917f03353993db4da101eb2a7d4dbdea6f8e0
gramosubuntu <gerard.ramosm89@gmail.com>
2016-12-20 14:43:45 -0800

    testing

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,26 +1,51 @@
 var React = require('react');
 var Router = require('react-router');
+var Repos = require('./Github/Repos');
+var Notes = require('./Notes/Notes');
+var UserProfile = require('./Github/UserProfile');
+var ReactFireMixin = require('reactfire');
+var Firebase = require('firebase');
+Firebase.initializeApp(
+{
+        apiKey: "AIzaSyCiIAx4C_n7RHR7jxoMtfNfF9ceJ7NFv1Q",
+            authDomain: "github-notetaker-e4eda.firebaseapp.com",
+                databaseURL: "https://github-notetaker-e4eda.firebaseio.com",
+                    storageBucket: "github-notetaker-e4eda.appspot.com",
+                        messagingSenderId: "902015046870"
+                            }
+);
 
 var Profile = React.createClass({
+  mixins: [ReactFireMixin],
   getInitialState: function(){
     return {
-      notes: [],
-      bio:{},
-      repos:[]
+      notes: [1,2,3],
+      bio:{
+        name: 'Gerry Ramos'
+      },
+      repos:['a','b','c']
     }
   },
+  componentDidMount: function(){
+    this.ref = Firebase.database();
+    var childRef = this.ref.child(this.props.params.username);
+    this.bindAsArray(childRef,'notes');
+
+  },
+  componentWillUnmount: function(){
+    this.unbind('notes');
+  },
   render: function(){
-  console.log(this.props);
   return (
       <div className="row">
         <div className="col-md-4">
-        User Profile Component --> { this.props.params.username }
+          <UserProfile username={ this.props.params.username} bio={this.state.bio}/>
         </div>
         <div className="col-md-4">
-          Repos Component
+          <Repos username={ this.props.params.username} repos={this.state.repos}/>
         </div>
         <div className="col-md-4">
-          Notes Component
+          <Notes username={ this.props.params.username} notes={this.state.notes}/>
         </div>
       </div>
   )
